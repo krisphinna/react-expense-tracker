0ab0a4180c16e7d5787c3a6c6339b3ccc3fde1a9
Kris Adelowo <krisphinna@gmail.com>
2022-01-27 04:47:02 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,82 @@
+import React, { useState, useEffect } from 'react';
+import { Jumbotron, Container } from 'reactstrap';
+
 import logo from './logo.svg';
 import './App.css';
+import Form from "./components/Form";
+import List from "./components/List";
+
+const ALL_EXPENSES = localStorage.getItem('expenses')
+  ? JSON.parse(localStorage.getItem('expenses'))
+  : []
+
 
 function App() {
+  const [expenses, setExpenses] = useState(ALL_EXPENSES)
+  const [name, setName] = useState('')
+  const [amount, setAmount] = useState('')
+  
+  const handleName = event => {
+    console.log('Name ', event.target.value)
+    setName(event.target.value)
+  }
+
+  const handleAmount = event => {
+    console.log('Amount ', event.target.value)
+    setAmount(event.target.value)
+  }
+
+  const handleSubmitForm = event => {
+    event.preventDefault()
+  }
+
+  const handleClearExpenses = () => {
+    setExpenses([])
+  }
+
+  useEffect(() => {
+    localStorage.setItem('expenses', JSON.stringify(expenses))
+  }, [expenses])
+  
+  if (name !== '' && amount > 0) {
+    const expense = { name, amount }
+    setExpenses([...expenses, expense])
+    
+    setName('')
+    setAmount('')
+  } else {
+    console.log('Invalid expense name or the amount')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container style={{ marginTop: 20}}>
+      <div className="container-fluid bg-light text-dark p-5">
+        <div className="container bg-light p-5">
+          <h3 className="display-6">
+            Expenses Tracker React App
+            <img src={logo} style={{ width: 50, height: 50 }} alt="react-logo" />
+          </h3>
+          <div>
+          Total Expenses: {' '}
+          <span className="text-success">
+            ${' '}
+            {expenses.reduce((accumulator, currentValue) => {
+              return (accumulator += parseInt(currentValue.amount))
+            }, 0)}
+          </span>
+          </div>
+          <Form
+            name={name}
+            amount={amount}
+            handleName={handleName}
+            handleAmount={handleAmount}
+            handleSubmitForm={handleSubmitForm}
+            handleClearExpenses={handleClearExpenses}          
+          />
+          <List expenses={expenses} />
+        </div>
+      </div>
+    </Container>    
   );
 }
 
